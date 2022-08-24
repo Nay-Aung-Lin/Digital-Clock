c3a2133761f5ecdead7c0ddee302c955f3658bf5
Nicky <Theoryt8@gmail.com>
2022-08-24 12:47:57 +0630

    responsive

--- app.js
+++ app.js
@@ -1,52 +1,46 @@
-'use strict';
-
-let clock = document.querySelector('.clock');
-
-let day = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
+"use strict";
+let clock = document.querySelector(".clock");
 
 let monthsName = [
-     "January", 
-     "February", 
-     "March",
-      "April", 
-     "May", 
-     "June", 
-     "July", 
-     "August", 
-     "September", 
-     "October", 
-     "November",
-      "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
-console.log(day[2]);
 
-function time() {
-    let date = new Date();
+let dayName = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-    let hours = date.getHours();
-    let min   = date.getMinutes();
-    let sec   = date.getSeconds();
-    let dayName = date.getDay();
-    let months = date.getMonth();
-    let dayNo = date.getDate();
-    let year = date.getFullYear();
+function run() {
+  let date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  let months = date.getMonth();
+  let day = date.getDate();
+  let dayOfWeek = date.getDay();
 
-    if(hours > 12) hours -= 12;
-    if(hours < 10) hours  = `0${hours}`;
-    if(min < 10 ) min = `0${min}`;
-    if(sec < 10 ) sec = `0${sec}`;
+  if (hour > 12) hour -= 12;
+  if (hour < 10) hour = "0" + hour;
+  if (min < 10) min = "0" + min;
+  if (sec < 10) sec = "0" + sec;
 
-    let details = `${day[dayName]},${monthsName[months]},${dayNo},${year}`;
-    clock.innerHTML = `${hours}:${min}:${sec}<br/> <span>${details}</span>`;
-
+  clock.innerHTML = `${hour} : ${min} : ${sec} <br/> ${monthsName[months]} ${day} ${dayName[dayOfWeek]}`;
 }
 
-setInterval(time,1000);
+setInterval(run, 1000);
